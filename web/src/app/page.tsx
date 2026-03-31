f9cdd519ac111280fa06dc18809ff96f79eb15da
Valerio van den Broek <valeriovandenbroek@gmail.com>
2026-03-31 09:24:17 +0200

    feat: change location distribution to stacked bar chart

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -169,7 +169,7 @@ export default function OverviewPage() {
         }
       >
         <Card>
-          <StatBar items={locationItems(overview.location)} showPct />
+          <StackedBar items={locationItems(overview.location)} />
         </Card>
       </Section>
 
